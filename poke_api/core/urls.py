22ecd4a03bf36ee1c220726ecce9bfd3464c3ae9
n-varvonets <nickolay.varvonets@gmail.com>
2023-02-06 00:33:05 +0200

    added bootstrap navbar and create mvc details of poke

--- poke_api/core/urls.py
+++ poke_api/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index_page, list_pokes_page
+from .views import index_page, list_pokes_page, pokemon_detail
 
 
 urlpatterns = [
     path('', index_page, name="index_page"),
     path('list_pokes/', list_pokes_page, name="list_pokes_page"),
+    path('poke/<slug:slug>/', pokemon_detail, name="pokemon_detail"),
 ]
--- poke_api/core/views.py
+++ poke_api/core/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Pokemon
 # Create your views here.
+
 
 def index_page(request):
     # return HttpResponse("Welcome to index page")
     base_html_variable_for_index = "base_html_variable_for_index_from_VIEW"
     return render(request, "base.html", {"base_html_variable_for_index": base_html_variable_for_index})
 
+
 def list_pokes_page(request):
     # return HttpResponse("Welcome to index page")
-    hardcore_list_of_pokes = "Slow-Nick-Poke"
-    return render(request, "list_poks.html", {"hardcore_list_of_pokes": hardcore_list_of_pokes})
+    hardcore_list_of_pokes = "Slow-Nick-Poke-Welcome from google"
+    list_of_pokes = Pokemon.objects.all().order_by("name")
+    # list_of_pokes = get_object_or_404(Pokemon)
+    return render(request, "list_poks.html", {"hardcore_list_of_pokes": hardcore_list_of_pokes, "list_of_pokes": list_of_pokes})
+
+
+def pokemon_detail(request, slug):
+    pokemon = get_object_or_404(Pokemon, slug=slug)
+    return render(request, 'details.html', {"pokemon": pokemon})
